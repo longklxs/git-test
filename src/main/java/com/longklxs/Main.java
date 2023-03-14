7655ab2130ab988e912bd1bbb7cf64cb0aa1ddd7
longklxs <longklxs@gmail.com>
2023-03-14 20:13:58 +0800

    push commit

--- src/main/java/com/longklxs/Main.java
+++ src/main/java/com/longklxs/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello hehel!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
+
 
     }
 }
